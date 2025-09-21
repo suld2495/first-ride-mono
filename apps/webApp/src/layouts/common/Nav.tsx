f3e40a169a941bf4a9658ca04e0879f6db7c2925
suld2495 <suld7777@gmail.com>
2025-09-21 12:44:05 +0900

    Feat: package 최신화

--- apps/webApp/src/layouts/common/Nav.tsx
+++ apps/webApp/src/layouts/common/Nav.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation, useNavigate } from 'react-router';
+import { Link, useLocation } from 'react-router';
 import { IconHome, IconFriends } from '@tabler/icons-react';
 import React from 'react';
 
