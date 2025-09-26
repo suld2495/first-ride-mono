0635ce4d4a9c7cde113608110f24d3b4e260a83b
suld2495 <suld7777@gmail.com>
2025-09-26 23:17:16 +0900

    Feat: Mock 업로드

--- apps/webApp/src/mocks/handlers/friends.ts
+++ apps/webApp/src/mocks/handlers/friends.ts
@@ -1,31 +1,69 @@
-import { Friend } from '@repo/types';
+import { Friend, FriendRequestResponse } from '@repo/types';
 import { http, HttpResponse } from 'msw';
 
-const BASE_URL = import.meta.env.VITE_MOCK_BASE_URL;
+const BASE_URL = `${import.meta.env.VITE_MOCK_BASE_URL}/api/friends`;
 
 export const friendsHandlers = [
-  http.get<object, object, Friend[]>(`${BASE_URL}/api/friend`, async () => {
-    return HttpResponse.json([
+  http.get<object, object, Friend[]>(`${BASE_URL}`, async () => {
+    return HttpResponse.json<Friend[]>([
       {
-        userId: 'yunji1',
-        nickname: '윤지1',
-        isFollowing: false,
+        nickname: '윤1',
+        job: '개발자',
+        profileImage: '',
+        friendSince: '',
       },
       {
-        userId: 'yunji2',
-        nickname: '윤지2',
-        isFollowing: true,
+        nickname: '윤2',
+        job: '개발자',
+        profileImage: '',
+        friendSince: '',
       }, 
     ]);
   }),
 
-  http.post<object, object>(`${BASE_URL}/api/friend/:id`, async () => {
+  // 친구 요청
+  http.post<object, object>(`${BASE_URL}/requests`, async () => {
     return HttpResponse.json(
       { status: 200 },
     );
   }),
 
-  http.delete<object, object>(`${BASE_URL}/api/friend/:id`, async () => {
+  // 친구 삭제
+  http.delete<object, object>(`${BASE_URL}/:id`, async () => {
+    return HttpResponse.json(
+      { status: 200 },
+    );
+  }),
+  
+  // 친구 요청 리스트
+  http.get<object, object, FriendRequestResponse[]>(`${BASE_URL}/requests`, async () => {
+    return HttpResponse.json<FriendRequestResponse[]>([
+      {
+        "id": 1,
+        "senderNickname": "yun2",
+        "receiverNickname": "문11",
+        "status": "PENDING",
+        "createdAt": "2025-09-24T00:18:14.047589"
+      },
+      {
+        "id": 2,
+        "senderNickname": "yun12",
+        "receiverNickname": "문11",
+        "status": "PENDING",
+        "createdAt": "2025-09-24T00:18:14.047589"
+      }, 
+    ]);
+  }),
+
+  // 친구 승인
+  http.post<object, object>(`${BASE_URL}/requests/:requestId/accept`, async () => {
+    return HttpResponse.json(
+      { status: 200 },
+    );
+  }),
+
+  // 친구 거절
+  http.post<object, object>(`${BASE_URL}/requests/:requestId/reject`, async () => {
     return HttpResponse.json(
       { status: 200 },
     );
--- apps/webApp/src/mocks/handlers/user.ts
+++ apps/webApp/src/mocks/handlers/user.ts
@@ -4,15 +4,15 @@ import { http, HttpResponse } from 'msw';
 const BASE_URL = import.meta.env.VITE_MOCK_BASE_URL;
 
 export const userHandlers = [
-  http.get<object, object, User[]>(`${BASE_URL}/api/user`, async () => {
+  http.get<object, object, User[]>(`${BASE_URL}/api/users/search`, async () => {
     return HttpResponse.json([
       {
-        userId: 'yunji1',
-        nickname: '윤지1',
+        userId: 'yun1',
+        nickname: '윤1',
       },
       {
-        userId: 'yunji2',
-        nickname: '윤지2',
+        userId: 'yun2',
+        nickname: '윤2',
       }, 
     ]);
   }),
