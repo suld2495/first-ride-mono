37690f1b9bf979f0101c8e9c6ba71ecab289d64e
suld2495 <suld7777@gmail.com>
2025-11-01 10:59:41 +0900

    Refactor: 벨 컴포넌트 추가가

--- apps/webApp/src/layouts/friend/FriendHeader.tsx
+++ apps/webApp/src/layouts/friend/FriendHeader.tsx
@@ -1,24 +1,60 @@
+import Button from "@/components/common/button/Button";
 import DarkMode from "@/components/common/DarkMode";
+import NotificationBell from "@/components/common/notification-bell/NotificationBell";
 import Paragraph from "@/components/common/paragraph/Paragraph";
-import { IconBellPlus } from "@tabler/icons-react";
-import Header from "../common/Header";
 import { useAcceptFriendRequestMutation, useFetchFriendRequestsQuery, useRejectFriendRequestMutation } from "@repo/shared/hooks/useFriend";
 import { useState } from "react";
+import Header from "../common/Header";
 import { getFormatDate } from "@repo/shared/utils";
-import { useShowModal } from "@/hooks/useModal";
-import Button from "@/components/common/button/Button";
+import { FriendRequest } from "@repo/types";
+
+const NotificationContent = (
+  { 
+    id, 
+    senderNickname, 
+    createdAt,
+    onAccept,
+    onReject,
+  }: FriendRequest & { 
+    onAccept: (id: number) => void; 
+    onReject: (id: number) => void; 
+  }
+) => {
+  return (
+    <div className={`flex flex-col`}>
+      <div className="flex justify-between">
+        <Paragraph className="text-gray-600 font-semibold">
+          {senderNickname}
+        </Paragraph>
+        <div className="flex gap-2">
+          <Button 
+            size="very-small" 
+            className="px-2 dark:bg-blue-400 dark:hover:bg-blue-500"
+            onClick={() => onAccept(id)}
+          >
+            추가
+          </Button>
+          <Button 
+            size="very-small" 
+            className="px-2 dark:bg-red-500 dark:hover:bg-red-600"
+            onClick={() => onReject(id)}
+          >
+            거절
+          </Button>
+        </div>
+      </div>
+      <div className="flex justify-between items-center mt-1 text-[12px] text-gray-500 dark:text-gray-300">
+        <span>{getFormatDate(createdAt)}</span>
+      </div>
+    </div>
+  )
+}
 
 const FriendHeader = () => {
   const [page] = useState(1);
-  const [show, setShow] = useShowModal(false);
   const { data: list } = useFetchFriendRequestsQuery(page);
   const acceptFriendMutation = useAcceptFriendRequestMutation();
   const rejectFriendRequestMutation = useRejectFriendRequestMutation();
-
-  const handleClick = (e: React.MouseEvent) => {
-    setShow(true);
-    e.stopPropagation();
-  };
 
   const handleAccpet = async (id: number) => {
     try {
@@ -42,60 +78,21 @@ const FriendHeader = () => {
     <Header>
       <Paragraph variant="h3">친구 리스트</Paragraph>
       <div className="flex gap-3 items-center">
-        <div
-          className="relative text-primary-color dark:text-white cursor-pointer flex "
-          onClick={handleClick}
-        >
-          <IconBellPlus stroke={2} />
-          {list.length > 0 && (
-            <span className="absolute -top-1 -right-1 w-[15px] h-[15px] leading-3 text-center rounded-full bg-red-400 text-[10px] text-white">
-              {list.length}
-            </span>
+        <NotificationBell
+          list={list.map((item) => ({
+            ...item,
+            title: item.senderNickname,
+          }))} 
+          renderItem={(item) => (
+            <NotificationContent 
+              onAccept={handleAccpet} 
+              onReject={handleReject}
+              {...item}
+            />
           )}
-        </div>
+        />
         <DarkMode />
       </div>
-      {show && !!list.length && (
-        <div className="absolute z-10 top-12 right-11">
-          <div className="fixed z-1 w-full h-full top-0 left-0"></div>
-          <ul className="alert relative z-2 w-[200px] p-2 border-[1px] border-gray-300 rounded-sm bg-white dark:bg-primary-color shadow-lg dark:shadow-middle dark:shadow-gray-600/80">
-            {list.map(
-              ({ id, senderNickname, createdAt }) => (
-                <li key={id}>
-                  <div
-                    className={`flex flex-col p-2 border-b-[1px] border-gray-200 rounded-sm`}
-                  >
-                    <div className="flex justify-between">
-                      <Paragraph className="text-gray-600 font-semibold">
-                        {senderNickname}
-                      </Paragraph>
-                      <div className="flex gap-2">
-                        <Button 
-                          size="very-small" 
-                          className="px-2 dark:bg-blue-400 dark:hover:bg-blue-500"
-                          onClick={() => handleAccpet(id)}
-                        >
-                          추가
-                        </Button>
-                        <Button 
-                          size="very-small" 
-                          className="px-2 dark:bg-red-500 dark:hover:bg-red-600"
-                          onClick={() => handleReject(id)}
-                        >
-                          거절
-                        </Button>
-                      </div>
-                    </div>
-                    <div className="flex justify-between items-center mt-1 text-[12px] text-gray-500 dark:text-gray-300">
-                      <span>{getFormatDate(createdAt)}</span>
-                    </div>
-                  </div>
-                </li>
-              ),
-            )}
-          </ul>
-        </div>
-      )}
     </Header>
   )
 };
--- apps/webApp/src/layouts/routine/RoutineHeader.tsx
+++ apps/webApp/src/layouts/routine/RoutineHeader.tsx
@@ -1,30 +1,43 @@
 import { RequestList } from '@repo/types';
-import { IconBellPlus } from '@tabler/icons-react';
 
 import DarkMode from '@/components/common/DarkMode';
+import NotificationBell from '@/components/common/notification-bell/NotificationBell';
 import Paragraph from '@/components/common/paragraph/Paragraph';
 import { ModalName, useModalStore } from '@/store/modal.store';
 import { useRequestStore } from '@repo/shared/store/request.store';
-import { getFormatDate } from '@repo/shared/utils';
 import Header from '../common/Header';
-import { useShowModal } from '@/hooks/useModal';
+import { getFormatDate } from '@repo/shared/utils';
 
 interface RoutineHeaderProps {
   list: RequestList;
   nickname: string;
 }
 
+const NotificationContent = ({
+  mateNickname, 
+  createdAt, 
+  routineName,
+}: { id: number; mateNickname: string; createdAt: string; routineName: string; }) => {
+  return (
+    <div
+      className={`flex flex-col hover:bg-gray-100 dark:hover:bg-dark-primary-color-hover rounded-sm cursor-pointer`}
+    >
+      <Paragraph className="text-gray-600 font-semibold">
+        {routineName}
+      </Paragraph>
+      <div className="flex justify-between items-center mt-1 text-[12px] text-gray-500 dark:text-gray-300">
+        <span>{mateNickname}</span>
+        <span>{getFormatDate(new Date(createdAt))}</span>
+      </div>
+    </div>
+  )
+};
+
 const RoutineHeader = ({ list, nickname }: RoutineHeaderProps) => {
-  const [show, setShow] = useShowModal(false);
   const showModal = useModalStore((state) => state.show);
   const setRequestId = useRequestStore((state) => state.setRequestId);
-
-  const handleClick = (e: React.MouseEvent) => {
-    setShow(true);
-    e.stopPropagation();
-  };
-
-  const handleModalShow = (id: number) => {
+  
+  const handleModalShow = ({ id }: { id: number }) => {
     showModal(ModalName.REQUEST_DETAIL);
     setRequestId(id);
   };
@@ -35,44 +48,20 @@ const RoutineHeader = ({ list, nickname }: RoutineHeaderProps) => {
         <Paragraph className="text-[16px]">{nickname}</Paragraph>
       </div>
       <div className="flex gap-3 items-center">
-        <div
-          className="relative text-primary-color dark:text-white cursor-pointer flex "
-          onClick={handleClick}
-        >
-          <IconBellPlus stroke={2} />
-          {list.length > 0 && (
-            <span className="absolute -top-1 -right-1 w-[15px] h-[15px] leading-3 text-center rounded-full bg-red-400 text-[10px] text-white">
-              {list.length}
-            </span>
+        <NotificationBell
+          list={list.map((item) => ({
+            ...item,
+            title: item.routineName,
+          }))} 
+          onClick={handleModalShow} 
+          renderItem={(item) => (
+            <NotificationContent 
+              {...item}
+            />
           )}
-        </div>
+        />
         <DarkMode />
       </div>
-      {show && !!list.length && (
-        <div className="absolute z-10 top-12 right-11">
-          <div className="fixed z-1 w-full h-full top-0 left-0"></div>
-          <ul className="relative z-2 w-[200px] p-2 border-[1px] border-gray-300 rounded-sm bg-white dark:bg-primary-color shadow-lg dark:shadow-middle dark:shadow-gray-600/80">
-            {list.map(
-              ({ id, routineName, nickname: mateNickname, createdAt }) => (
-                <li key={id}>
-                  <div
-                    className={`flex flex-col p-2 border-b-[1px] border-gray-200 hover:bg-gray-100 dark:hover:bg-dark-primary-color-hover rounded-sm cursor-pointer`}
-                    onClick={() => handleModalShow(id)}
-                  >
-                    <Paragraph className="text-gray-600 font-semibold">
-                      {routineName}
-                    </Paragraph>
-                    <div className="flex justify-between items-center mt-1 text-[12px] text-gray-500 dark:text-gray-300">
-                      <span>{mateNickname}</span>
-                      <span>{getFormatDate(new Date(createdAt))}</span>
-                    </div>
-                  </div>
-                </li>
-              ),
-            )}
-          </ul>
-        </div>
-      )}
     </Header>
   );
 };
