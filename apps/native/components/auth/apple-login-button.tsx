714982bfe4be31dade2edc337005311a0e8dff67
hy0909 <hy0909@users.noreply.github.com>
2026-07-26 19:01:22 +0900

    Move Apple login logo left

--- apps/native/components/auth/apple-login-button.tsx
+++ apps/native/components/auth/apple-login-button.tsx
@@ -103,7 +103,7 @@ const styles = StyleSheet.create((theme) => ({
   },
   logo: {
     position: 'absolute',
-    left: baseFoundation.dimension.x2,
+    left: baseFoundation.dimension.x0,
     width: baseFoundation.dimension.x20,
     height: baseFoundation.dimension.x20,
     paddingBottom: baseFoundation.dimension.x1,
