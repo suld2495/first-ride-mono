e26c6f6a0e1f71e3face2925e842d1763beb192a
suld2495 <suld7777@gmail.com>
2026-08-12 10:56:10 +0900

    refactor: format contained background assertion

--- apps/native/__tests__/app/friend.test.tsx
+++ apps/native/__tests__/app/friend.test.tsx
@@ -118,9 +118,10 @@ describe('친구 리스트 페이지', () => {
       expect(await screen.findByTestId('random-friend-card')).toHaveStyle({
         height: baseFoundation.dimension.x250,
       });
-      expect(
-        await screen.findByTestId('random-friend-background'),
-      ).toHaveProp('resizeMode', 'contain');
+      expect(await screen.findByTestId('random-friend-background')).toHaveProp(
+        'resizeMode',
+        'contain',
+      );
       expect(await screen.findByTestId('random-friend-character')).toHaveStyle({
         width: baseFoundation.dimension.x140,
         height: baseFoundation.dimension.x140,
